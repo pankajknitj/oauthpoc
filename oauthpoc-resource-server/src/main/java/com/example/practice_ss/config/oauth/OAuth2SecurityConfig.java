3ce180aa7a41ddcbe9683c14aa4071b441866f2f
pankajknitj <pkofficial52@gmail.com>
2025-11-20 22:53:07 +0530

    oauth2 login setup

--- oauthpoc-resource-server/src/main/java/com/example/practice_ss/config/oauth/OAuth2SecurityConfig.java
+++ oauthpoc-resource-server/src/main/java/com/example/practice_ss/config/oauth/OAuth2SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.example.practice_ss.config.oauth;
 
 import com.example.practice_ss.exception.CustomBassicAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,6 +22,8 @@ import static com.example.practice_ss.constans.Paths.*;
 @Profile(OAUTH2)
 @Configuration
 public class OAuth2SecurityConfig {
+    @Autowired
+    Environment environment;
 
     @Bean
     SecurityFilterChain CustomSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -26,6 +36,10 @@ public class OAuth2SecurityConfig {
                 .requestMatchers(MY_CARDS.toString()).hasRole("CARDS_READ")
                 .requestMatchers(MY_BALANCE.toString(),MY_LOANS.toString(),USER.toString()).authenticated()
         );
+        http.oauth2Login(oc -> oc.defaultSuccessUrl("/myCards",true)
+                .userInfoEndpoint(userInfo ->
+                        userInfo.oidcUserService(new KeycloakOIDCUserService())
+                ));
 
         http.oauth2ResourceServer(orc -> orc.jwt(jwtConfigurer ->
                 jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter)));
@@ -33,5 +47,37 @@ public class OAuth2SecurityConfig {
         http.csrf(csrf -> csrf.disable()) ; // Disable CSRF for H2 console
         http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
         return http.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistration() {
+        return new InMemoryClientRegistrationRepository(keyckloakClientRegistration());
+    }
+
+    @Bean
+    public ClientRegistration keyckloakClientRegistration() {
+        String clientId = environment.getProperty("application.security.keycloak.client-id");
+        String clientSecret = environment.getProperty("application.security.keycloak.client-secret");
+        String tokenUri = environment.getProperty("application.security.keycloak.token-uri");
+        String authUri = environment.getProperty("application.security.keycloak.auth-uri");
+        String issuerUri = environment.getProperty("application.security.keycloak.issuer-uri");
+        String userInfoUri = environment.getProperty("application.security.keycloak.userInfo-uri");
+        String jwkSetUri = environment.getProperty("application.security.keycloak.jwks-uri");
+        String redirectUri = "{baseUrl}/login/oauth2/code/{registrationId}";
+
+        return ClientRegistration.withRegistrationId("KEYCLOAK")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri(redirectUri)
+                .scope(new String[]{"openid", "email"})
+                .issuerUri(issuerUri)
+                .userInfoUri(userInfoUri)
+                .jwkSetUri(jwkSetUri)
+                .userNameAttributeName("preferred_username")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .tokenUri(tokenUri)
+                .authorizationUri(authUri)
+                .build();
     }
 }
--- oauthpoc-resource-server/src/main/java/com/example/practice_ss/controller/MyBalanceController.java
+++ oauthpoc-resource-server/src/main/java/com/example/practice_ss/controller/MyBalanceController.java
@@ -3,15 +3,17 @@ package com.example.practice_ss.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyBalanceController {
-    @PreAuthorize("hasAuthority('BALANCE_READ')")
+//    @PreAuthorize("hasAuthority('BALANCE_READ')")
     @GetMapping("/myBalance")
-    public ResponseEntity<String> getBalance(){
+    public ResponseEntity<String> getBalance(Authentication authentication){
+        System.out.println(authentication);
         return new ResponseEntity<>("Balance is working", HttpStatus.ACCEPTED);
     }
 }
